061306809a23f9c2e25ff1116304f655cbd3ed06
Nivetha V <98044718+nivi30@users.noreply.github.com>
2025-11-10 14:30:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import FloatComp from './FloatComp';
+// import Cardview from './Cardview';
+// import Sideview from './Sideview';
+// import Announcement from "./VerticalAnnouncementTab";
+// import Sidebar from "./Sidebar";
+// import { Grid } from "@mui/material";
+// import ResponsiveGridChart from './ResponsiveGridChart';
+import UserMetrics from "./UserMetrics";
+
+export const App = () => {
+
+
+  return (
+    <>
+    <UserMetrics />
+    {/* <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} md={3}>
+        <ResponsiveGridChart />
+      </Grid>
+    </Grid> */}
+    {/* <Sidebar /> */}
+      {/* <Announcement /> */}
+      {/* <Cardview />
+      <Sideview />
+      <FloatComp /> */}
+    </>
+  );
+};
+
+export default App;
